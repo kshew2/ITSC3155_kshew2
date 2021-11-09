70b6f9e415ef84e2c04a797f6d87286f663eee24
Khalil Shew <kshew2@uncc.edu>
2021-11-09 09:39:25 -0500

    flask app initial commit

--- flask_app/flask01.py
+++ flask_app/flask01.py
@@ -1,4 +1,4 @@
-# FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
+            # Ex1 -- We show the bare bones code to get an app up and running
 
 # imports
 import os                 # os is used to get environment variables IP & PORT
@@ -6,19 +6,30 @@ from flask import Flask   # Flask is the web app that we will customize
 
 app = Flask(__name__)     # create an app
 
-# @app.route is a decorator. It gives the function "index" special powers.
+# @app.route is a decorator. It gives the function "hello" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/')
-def index():
-    return 'Welcome, Notes App User!'
+def hello():
+    return 'Hello World!'
 
+app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+# Run this in cloud9 by clicking on the green play button labeled Run.
 
-# To see the web page in your web browser, go to the url,
-#   http://127.0.0.1:5000
+# Also, make sure you can run this with
+#    python ex1.py
+# from the command line
+
+# To see the web page in another tab, go to the url, which is of the form
+#   <workspace-name>-<user-name>.c9users.io:8080
+# This is referred to as <url> below.
 
 # Note that we are running with "debug=True", so if you make changes and save it
 # the server will automatically update. This is great for development but is a
 # security risk for production.
+
+# To Do:
+#  1. Change the return statement in the function and see what happens
+#  2. Change the @app.route('/') to @app.route('/<something-else>') and see what
+#     happens when you go to <url>:/ and <url>:/<something>
